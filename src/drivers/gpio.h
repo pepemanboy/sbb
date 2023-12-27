c68ecb7ce908ae1b9c18063f53dd57d7a59b67ca
Pepe Melendez <pepe_ciro@hotmail.com>
2023-12-27 10:41:21 -0600

    Create Gpio driver.
    Simple GPIO interface and its respective Arduino implementation.

---
+++ src/drivers/gpio.h
@@ -0,0 +1,30 @@
+#ifndef SBB_DRIVERS_GPIO_H
+#define SBB_DRIVERS_GIPO_H
+
+namespace sbb {
+
+enum class GpioMode {
+  kInput,
+  kOutput,
+};
+
+enum class GpioPolarity {
+  kActiveHigh,
+  kActiveLow,
+};
+
+struct GpioPin {
+  GpioMode mode;
+  GpioPolarity polarity;
+  int pin;
+};
+
+void GpioConfigure(const GpioPin &gpio);
+
+void GpioSet(const GpioPin &gpio, bool state);
+
+bool GpioGet(const GpioPin &gpio);
+
+}  // namespace sbb
+
+#endif  // SBB_DRIVERS_GIPO_H
---
+++ src/drivers/gpio_arduino.cc
@@ -0,0 +1,28 @@
+#include <Arduino.h>
+
+#include "drivers/gpio.h"
+
+namespace sbb {
+
+void GpioConfigure(const GpioPin &gpio) {
+  switch (gpio.mode) {
+    case GpioMode::kInput:
+      pinMode(gpio.pin, INPUT);
+      break;
+    case GpioMode::kOutput:
+      pinMode(gpio.pin, OUTPUT);
+      break;
+  }
+}
+
+void GpioSet(const GpioPin &gpio, bool state) {
+  if (gpio.polarity == GpioPolarity::kActiveHigh) {
+    digitalWrite(gpio.pin, state ? HIGH : LOW);
+  } else {  // GpioPolarity::kActiveLow
+    digitalWrite(gpio.pin, state ? LOW : HIGH);
+  }
+}
+
+bool GpioGet(const GpioPin &gpio) { return digitalRead(gpio.pin); }
+
+}  // namespace sbb
