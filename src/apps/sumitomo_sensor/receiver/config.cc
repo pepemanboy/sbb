c2dd51edbd3e886bd5918efb04751e17de136d66
Pepe Melendez <pepemanboy@gmail.com>
2024-10-31 19:01:46 -0700

    Change IDs used for this batch.

--- src/apps/sumitomo_sensor/receiver/config.cc
+++ src/apps/sumitomo_sensor/receiver/config.cc
@@ -18,8 +18,7 @@ BitArray<128> NodeAddressMask(const int (&bits)[N]) {
 
 ReceiverConfig GetConfig() {
   return {.node_address_mask =
-              NodeAddressMask({1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14,
-                               15, 16, 17, 18, 19})};
+              NodeAddressMask({1, 2, 3, 4, 5, 6, 7, 8, 9, 10})};
 }
 
 }  // namespace receiver
